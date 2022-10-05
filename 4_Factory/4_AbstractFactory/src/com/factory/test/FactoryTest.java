aaaad0d09a35faf58de42a348fdb76dfb71deb3c
plms <askdell@sina.com>
2022-10-05 21:27:47 +0800

    add

--- 4_Factory/4_AbstractFactory/src/com/factory/test/FactoryTest.java
+++ 4_Factory/4_AbstractFactory/src/com/factory/test/FactoryTest.java
@@ -8,7 +8,8 @@ import com.factory.order.*;
 * 1，抽象工厂模式将简单工厂模式和工厂方法模式进行了改进，或者称作进一步的抽象；
 * 2，这种模式将工厂抽象成两层，程序员可以根据需要的类来创建不用的工厂，如本例中需要corrolla则创建ToyotaFactory.java，
 * 需要civic则创建HondaFactory.java。
-* 3，这样将单个的简单工厂变成了工厂簇，便于维护了
+* 3，这样将单个的简单工厂变成了工厂簇，如果我需要大众品牌的车，则新建一个VolksWagon.java继承AbstractFactory.java
+* 在这个工厂类里写生产线管车型的代码即可，这样扩展性更好，便于维护了。
 */
 
 public class FactoryTest {
